59e80302420df54be37febf0f30b32c6896e4fd2
Enzoputachi <samuelnzoputachi@gmail.com>
2025-05-04 12:40:42 +0100

    Define services for user

--- src/services/userService.js
+++ src/services/userService.js
@@ -1,5 +1,38 @@
 import prisma from "../models/index.js";
 
+const createUser = async(validatedUserdata) => {
+    return await prisma.user.create({ data: validatedUserdata })
+}
+
+const getUser = async(userId = '') => {
+
+    if (userId) {
+        return await prisma.user.findUnique({
+            where: {id: userId}
+        })
+    }
+
+    return await prisma.user.findMany();
+}
 
 
+const updateUser = async(userId, data) => {
+    return await prisma.user.update({
+        where: { id: userId },
+        data,
+    })
+}
 
+
+const deleteUser = async(userId) => {
+    return await prisma.user.delete({
+        where: { id: userId }
+    })
+}
+
+export {
+    createUser,
+    getUser,
+    updateUser,
+    deleteUser,
+}
