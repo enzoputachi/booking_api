69c4e7364be27ee5bb629636096c331a042d9ecd
Enzoputachi <samuelnzoputachi@gmail.com>
2025-05-05 08:01:42 +0100

    feat: define helper function to generate seat number; Make createSeat part of a transaction

--- src/services/seatService.js
+++ src/services/seatService.js
@@ -1,7 +1,21 @@
 import prisma from "../models/index.js";
 
-const createSeat = async(validatedSeatdata) => {
-    return await prisma.seat.create({ data: validatedSeatdata })
+const createSeat = async(validatedSeatdata, client = prisma) => {
+    const { tripId, capacity, seatsPerRow } = validatedSeatdata;
+    const seatNumbers = generateSeatNumbers(capacity, seatsPerRow)
+    
+    const seats = seatNumbers.map(seatNumber => ({
+        tripId: tripId,
+        seatNo: seatNumber,
+        status: 'AVAILABLE'
+    }))
+
+    try {
+        return await client.seat.createMany({ data: seats, skipDuplicates: true })
+    } catch (error) {
+        console.error("Failed to create seats:", err.message);
+        throw err;
+    }
 }
 
 const getSeat = async(seatId = '') => {
@@ -28,6 +42,23 @@ const deleteSeat = async(seatId) => {
     return await prisma.seat.delete({
         where: { id: seatId }
     })
+}
+
+// Helper function to generate seat number
+const generateSeatNumbers = (capacity = 18, seatsPerRow = 4) => {
+    const seats = [];
+    const rows = Math.ceil(capacity / seatsPerRow)
+
+    for (let row = 0; row < rows; row++) {
+        const rowLetter = String.fromCharCode(65 + row);
+
+        for (let col = 1; col <=  seatsPerRow; col++) {
+            seats.push(`${rowLetter}${col}`)
+            if (seats.length === capacity) return seats;
+        }
+    }
+
+    return seats;
 }
 
 export {
