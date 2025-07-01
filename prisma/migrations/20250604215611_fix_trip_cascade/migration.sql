4f24e89e72606157867476ce99fe391a539b2b0a
Enzoputachi <samuelnzoputachi@gmail.com>
2025-07-01 22:35:37 +0100

    reset migration history

--- prisma/migrations/20250604215611_fix_trip_cascade/migration.sql
+++
@@ -1,8 +0,0 @@
--- DropForeignKey
-ALTER TABLE `seat` DROP FOREIGN KEY `Seat_tripId_fkey`;
-
--- DropIndex
-DROP INDEX `Seat_tripId_fkey` ON `seat`;
-
--- AddForeignKey
-ALTER TABLE `Seat` ADD CONSTRAINT `Seat_tripId_fkey` FOREIGN KEY (`tripId`) REFERENCES `Trip`(`id`) ON DELETE CASCADE ON UPDATE CASCADE;
--- prisma/migrations/20250628084936_add_whats_app_url/migration.sql
+++
@@ -1,2 +0,0 @@
--- AlterTable
-ALTER TABLE `companysetting` ADD COLUMN `whatsAppUrl` VARCHAR(191) NULL;
