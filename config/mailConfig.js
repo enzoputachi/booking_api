310c68c92d034ca468a6eba9cdc20f9a9ac6ab61
Enzoputachi <samuelnzoputachi@gmail.com>
2025-07-07 08:34:30 +0100

    update smtp

--- config/mailConfig.js
+++ config/mailConfig.js
@@ -7,7 +7,7 @@ pathFinder();
 export const transporter = nodemailer.createTransport({
     host: process.env.SMTP_HOST,
     port: +process.env.SMTP_PORT,
-    secure: 'true',
+    secure: 'fasle',
     auth: {
         user: process.env.SMTP_USER,
         pass: process.env.SMTP_PASS,
