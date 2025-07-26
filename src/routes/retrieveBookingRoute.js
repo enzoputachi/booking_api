13d49651761c22c4e8b75a131cd3331b78a6b145
Enzoputachi <samuelnzoputachi@gmail.com>
2025-07-26 17:23:09 +0100

    add retrieve booking route

--- src/routes/retrieveBookingRoute.js
+++ src/routes/retrieveBookingRoute.js
@@ -1,7 +1,9 @@
 import express from 'express';
 import { handleRetrieveBooking } from '../controllers/retrieveBookingController.js';
+import { handleConfirmBookingByToken } from '../controllers/bookingController.js';
 const router = express.Router();
 
-router.get('/', handleRetrieveBooking)
+router.get('/', handleRetrieveBooking);
+router.get('/confirm', handleConfirmBookingByToken); // Assuming you want to use the same handler for confirmation
 
 export default router;
